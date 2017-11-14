7a62cbf08a7d9c20aa6258bc6b7ec61a13805de2
JeonghyunYoo <yoojh9@hancomm.com>
2017-11-14 15:25:47 +0900

    toByteArray() 추가

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 
 1. <a href="/src/main/java/yjh/utility/apache/commons/io/IOUtils/"> org.apache.commons.io.IOUtils </a>
  - 일반적인 IO 스트림 조작 유틸리티
- 	- closeQuietly : null 및 예외를 무시하고 스트림을 close 함.
-	- copy : 하나의 스트림에 있는 데이터를 다른 스트림으로 복사.
+ 	- closeQuietly() : null 및 예외를 무시하고 스트림을 close 함.
+	- copy() : 하나의 스트림에 있는 데이터를 다른 스트림으로 복사.
---
+++ src/main/java/yjh/utility/apache/commons/io/IOUtils/ToByteArray.java
@@ -0,0 +1,26 @@
+package yjh.utility.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
+public class ToByteArray {
+
+	public static void main(String[] args) {
+		
+		try {
+			FileInputStream in = new FileInputStream("foo.txt");
+			byte[] byteArr = IOUtils.toByteArray(in);
+			System.out.println(byteArr.length);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+
+	}
+
+}
