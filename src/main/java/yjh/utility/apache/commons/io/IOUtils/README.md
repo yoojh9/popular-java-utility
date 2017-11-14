6040c7f86ef1e554d957fc7ebfdba53d2fa5b083
JeonghyunYoo <yoojh9@hancomm.com>
2017-11-14 15:11:46 +0900

    toString() 기능 추가

--- src/main/java/yjh/utility/apache/commons/io/IOUtils/README.md
+++ src/main/java/yjh/utility/apache/commons/io/IOUtils/README.md
@@ -1,5 +1,10 @@
 # org.apache.commons.io.IOUtils
 
+https://commons.apache.org/proper/commons-io/javadocs/api-2.5/org/apache/commons/io/IOUtils.html
+
 1. <a href="./CloseQuietly.java"> closeQuietly() </a>
  - 일반적으로 finally 블록에 사용된다. 일반적인 close 구문에 도달하기 전에 예외가 발생하더라도 close 시켜준다. 예외가 발생하지 않았을 때 일반적으로 사용하는 close()를 대체하여 사용해서는 안된다.
  
+1. <a href="./ToString.java"> toString() </a>
+ - InputStream, Reader, URI로 주어진 contents 등을 String으로 얻을 수 있다.
+ - Reader 타입일 경우 내부적으로 buffer를 지원하므로 BufferedReader를 따로 사용할 필요는 없다.
---
+++ src/main/java/yjh/utility/apache/commons/io/IOUtils/ToString.java
@@ -0,0 +1,26 @@
+package yjh.utility.apache.commons.io.IOUtils;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
+public class ToString {
+
+	public static void main(String[] args) {
+		
+		try {
+			FileReader in = new FileReader("foo.txt");
+			String input = IOUtils.toString(in);
+			System.out.println(input);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+
+	}
+
+}
